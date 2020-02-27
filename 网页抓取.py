df8a3fb213828f49f3590f253a7d3d1b1eeceb82
wang-tong <mr.wangtong@live.cn>
2020-02-27 21:36:21 +0800

    00

--- 网页抓取.py
+++ 网页抓取.py
@@ -8,6 +8,6 @@ t = bs_obj.find_all('table')[1]
 alltr = t.find_all('tr')
 alltr.pop(0)
 for r in alltr:
-    alltd=r.find_all('td')
+    alltd = r.find_all('td')
     print(f'{alltd[0].text} 卖出价 {alltd[4].text}')
 # %%
