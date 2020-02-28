7fc9aaa7ef0bae9052fc3f341431b530cb651950
wang-tong <mr.wangtong@live.cn>
2020-02-28 12:56:53 +0800

    0

--- pinyin tk interface.py
+++ pinyin tk interface.py
@@ -1,16 +1,19 @@
 from tkinter import Tk, Entry, Button
 import pinyin
+
+
 def convert():
-    s=ent_hanzi.get()
-    p=pinyin.get(s)
-    ent_pinyin.insert('end',p)
+    s = ent_hanzi.get()
+    p = pinyin.get(s)
+    ent_pinyin.insert('end', p)
     return
+
 
 fm_main = Tk()
 
 ent_hanzi = Entry(fm_main)
 ent_pinyin = Entry(fm_main)
-btn = Button(fm_main, text='显示拼音',command =convert)
+btn = Button(fm_main, text='显示拼音', command=convert)
 
 ent_hanzi.pack()
 btn.pack()
