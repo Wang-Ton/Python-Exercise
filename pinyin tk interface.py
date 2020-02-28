a6dc4a3eef1b6efdd6b357b2ef7991e191dcc325
wang-tong <mr.wangtong@live.cn>
2020-02-28 12:56:10 +0800

    0

---
+++ pinyin tk interface.py
@@ -0,0 +1,19 @@
+from tkinter import Tk, Entry, Button
+import pinyin
+def convert():
+    s=ent_hanzi.get()
+    p=pinyin.get(s)
+    ent_pinyin.insert('end',p)
+    return
+
+fm_main = Tk()
+
+ent_hanzi = Entry(fm_main)
+ent_pinyin = Entry(fm_main)
+btn = Button(fm_main, text='显示拼音',command =convert)
+
+ent_hanzi.pack()
+btn.pack()
+ent_pinyin.pack()
+
+fm_main.mainloop()
---
+++ 网页抓取 interface.py
@@ -0,0 +1,29 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+from tkinter import Tk, Button, Text
+
+
+def download():
+    c = urlopen('https://www.boc.cn/sourcedb/whpj/').read()
+    bs_obj = BeautifulSoup(c, features='lxml')
+    alltr = bs_obj.find_all('table')[1].find_all('tr')
+    for r in alltr[1:]:
+        alltd = r.find_all('td')
+        t1.insert('end', f'{alltd[0].text} 卖出价 {alltd[4].text}\n')
+
+
+def save():
+    f = open('d:/currency.txt', 'w')
+    f.write(t1.get(1.0, 'end'))
+    f.close()
+
+
+fm_main = Tk()
+t1 = Text(fm_main)
+b1 = Button(fm_main, text='获取外汇牌价', command=download)
+b2 = Button(fm_main, text='保存到文件', command=save)
+
+t1.pack()
+b1.pack()
+b2.pack()
+fm_main.mainloop()
