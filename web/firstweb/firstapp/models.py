967c240417e7868560479e144ec17e3519ed35b1
wang-tong <mr.wangtong@live.cn>
2020-03-14 15:48:10 +0800

    0

---
+++ web/firstweb/firstapp/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+
+class class1(models.Model):
+    buy_date = models.DateField(null=False,blank=False)
+    def __str__(self):
+        return self.name
