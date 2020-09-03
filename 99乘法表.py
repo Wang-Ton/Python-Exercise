00b46a0a9b8c33cc2fc5ec4da0a9e5d7e4fdb9f7
Wang-Ton <mr.wangtong@live.cn>
2020-09-03 22:54:30 +0800

    1

--- 99乘法表.py
+++ 99乘法表.py
@@ -1,21 +1,20 @@
-#%%
+# %%
 r = 1
 while r <= 9:
-    c=1
-    while c<=r:
+    c = 1
+    while c <= r:
         result = c*r
-        print(f'{c}*{r}={result}\t',end="")
-        c+=1
-    r+=1
+        print(f'{c}*{r}={result}\t', end="")
+        c += 1
+    r += 1
     print()
 
 
-
 # %%
-for r in range (1,10):
-    for c in range(1,r+1):
+for r in range(1, 10):
+    for c in range(1, r+1):
         result = c*r
-        print(f'{c}*{r}={result}\t',end="")
+        print(f'{c}*{r}={result}\t', end="")
     print()
 
 # %%
