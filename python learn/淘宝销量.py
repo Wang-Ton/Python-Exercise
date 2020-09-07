723820eb72d533dd3c9fccbd22108110730b6233
Wang-Ton <mr.wangtong@live.cn>
2020-09-07 21:30:04 +0800

    0

--- python learn/淘宝销量.py
+++ python learn/淘宝销量.py
@@ -1,2 +1,20 @@
+from sklearn.utils import resample
+from astropy.tests.runner import keyword
 import requests
 import bs4
+
+
+def open_url(keywords):
+    return res
+
+
+def main():
+    keywords = input("请输入关键词:")
+    res = open_url(keywords)
+
+    with open("d:\关键词.txt", "w", encoding="utf-8") as file:
+        file.write(res.text)
+
+
+if __name__ == "__main__":
+    main()
